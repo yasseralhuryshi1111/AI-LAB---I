ad3bef7460d5af08377f7709dc2984d0e3272cb9
Shaqra University <ccitlms@gmail.com>
2022-09-20 13:30:24 +0300

    DFS Completed!

--- DFS-AI-LAB1.py
+++ DFS-AI-LAB1.py
@@ -1,5 +1,5 @@
 # Using a Python dictionary to act as an adjacency list
-graph = {
+graph = {           #Defining a graph in this way! 
   '5' : ['3','7'],
   '3' : ['2', '4'],
   '7' : ['8'],
@@ -15,35 +15,23 @@ def dfs(visited, graph, node):  #function for dfs
         print (node)
         visited.add(node)
         for neighbour in graph[node]:
-            dfs(visited, graph, neighbour)
-
+            dfs(visited, graph, neighbour)  #Recusion! 
 # Driver Code
 print("Following is the Depth-First Search")
-dfs(visited, graph, '3')
-
+dfs(visited, graph, '8')
 
 # PSEUDOCODE FOR THE DFS
-
 '''
-DFS(G, u)
-
+DFS(G, u)                   #There is a Graph and Unvisited List[]
     u.visited = true
-
     for each v ∈ G.Adj[u]
-
         if v.visited == false
-
-            DFS(G,v)   
-
+                               #Same function calling himself again
+            DFS(G,v)            # Recursion is used ! 
 init() {
-
     For each u ∈ G
-
         u.visited = false
-
      For each u ∈ G
-
        DFS(G, u)
-
 }
 '''
